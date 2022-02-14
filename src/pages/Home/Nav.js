801cbb2b65b04a84c1dfdc65dc636af4065b3ff0
muskaan <muskaansuri004@gmail.com>
2022-02-14 14:08:52 +0530

    add nav bar

---
+++ src/pages/Home/Nav.js
@@ -0,0 +1,19 @@
+
+ 
+ const Nav = () => {
+    return (
+      <>
+        <div className="nav">
+          <img src='/assets/images/logoimg.png' alt="image"></img> 
+          <div className="nav-title">
+          <p id="home-title">Home</p>
+          <p>Product</p>
+          <p>Faq</p>
+          <p>Contact</p> 
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default Nav;
