a5ff86fb1bc26d1941fab23e0d95838dcc4913e8
Pratik Singh <singhpratik0001@gmail.com>
2022-02-16 18:09:32 +0530

    Mobile Responsive

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <AppWorking />
+      {/* <AppWorking /> */}
     </div>
   );
 }
--- src/pages/home/banner.js
+++ src/pages/home/banner.js
@@ -5,84 +5,92 @@ import styled from 'styled-components';
 const Banner = () => {
     return(
         <div className="banner">
-            <ButtonsWrapper>
-                <Button primary>Playstore</Button>
-                <Button>Appstore</Button>
-            </ButtonsWrapper>
-            <App>Food app</App>
+           <BannerImage src="/assets/images/banner_image.png"></BannerImage>
+           <App>Food app</App>
            <Title>Why stay hungry when 
                you can order form Bella Onojie
            </Title>
            <Description>Download the bella onoje’s food app now on</Description>
-           <BannerImage src="/assets/images/banner_image.png"></BannerImage>
-           <ImageContainer1>
-           <PhoneImage1 src="/assets/images/Phone1.png"></PhoneImage1> 
-           </ImageContainer1> 
-           {/* <ImageContainer2>
-           <PhoneImage2 src="/assets/images/Phone2.png"></PhoneImage2>
-           </ImageContainer2> */}
+           <ButtonsWrapper>
+           <Button>Playstore</Button>
+           <ColoredButton>Appstore</ColoredButton>
+           </ButtonsWrapper>
+           <PhoneImage1 src="/assets/images/Group 53.png"></PhoneImage1>
         </div>
     );
 };
 
-const ImageContainer1 = styled.div`
-display: flex;
-// justify-items: center;
-align-items: center;
-top: 45%;
-left: 20%;
-position: absolute;
-@media (max-width: 768px){
-    top: 50%;
-    right: 30%    
-}
-`;
-const ImageContainer2 = styled.div`
-display: flex;
-// justify-items: center;
-align-items: center;
-top: 40px;
-right: 90%;
-left: 0%;
-position: absolute;
-
-@media (max-width: 768px){
-    top: 3%;
-    right: 100%    
-}
-`;
-
 const ButtonsWrapper = styled.div`
-display: flex;
-// justify-items: center;
-align-items: center;
-padding-left: 40%;
-top: 50%;
 position: absolute;
-@media (max-width: 768px){
-    top: 50%;
-    right: 30%
+left: 35%;
+right: 25%;
+top: 50%;
+bottom: 50%;
+display: block;
+justify-items: center;
+align-items: center;
+position: absolute;
+
+@media (max-width: 414px){
+    left: 20%;
+    right: 25%;
+    top: 40%;
+    bottom: 50%;
+    display: block;
+    flex-direction: column;
+    align-items: flex-start;
 }
 `;
+
 
 const Button = styled.button`
-/* Adapt the colors based on primary prop */
-background: ${props => props.primary ? "var(--primary-color)" : "white"};
-color: ${props => props.primary ? "white" : "var(--primary-color)"};
-font-size: 1em;
-margin: 1em;
-padding: 0.25em 1em;
-border: 2px solid #FA4A0C;
+background-color: var(--primary-color);
+//  padding: 25px;
+border: none;
+color: white;
+padding: 16px 58.5px;
+text-align: center;
+text-decoration: none;
+display: in-block;
+font-size: 24px;
 border-radius: 30px;
 box-shadow: 0px 10px 30px rgba(183, 50, 39, 0.2);
 
+@media (max-width: 414px){
+ background-color: var(--primary-color);
+//  padding: 25px;
+border: none;
+color: white;
+padding: 16px 58.5px;
+text-align: center;
+text-decoration: none;
+display: in-block;
+font-size: 24px;
+border-radius: 30px;
+box-shadow: 0px 10px 30px rgba(183, 50, 39, 0.2);
+}
 `;
+
+const ColoredButton = styled.button`
+color: white;
+padding: 16px 58.5px;
+text-align: center;
+text-decoration: none;
+display: in-block;
+font-size: 24px;
+border-radius: 30px;
+background-color: white;
+color: var(--primary-color);
+border: 2px solid var(--primary-color); /* Green */
+`
 
 const App = styled.h3`
 position: absolute;
 left: 44.08%;
 right: 44.08%;
 top: 5%;
+bottom: 88.96%;
+
 
 /* H3 */
 
@@ -98,12 +106,36 @@ letter-spacing: 0.1px;
 
 color: #F7F7F7;
 
-@media (max-width: 768px){
-    left: 20%;
-    right: 20%;
-    text-size:auto;
-    top: 0px;
-    bottom: 88.96%;
+@media (max-width: 415px){
+position: static;
+left: 40.27%;
+right: 40.27%;
+top: 0%;
+bottom: 91.49%;
+
+/* H5 */
+
+font-family: Montserrat;
+font-style: normal;
+font-weight: bold;
+font-size: 16px;
+line-height: 24px;
+/* identical to box height, or 150% */
+
+text-align: center;
+letter-spacing: 0.1px;
+
+/* second text */
+
+color: #737373;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+margin: 7px 0px;
 }
 `;
 
@@ -111,8 +143,9 @@ const Description = styled.h2`
 position: absolute;
 left: 12.23%;
 right: 12.09%;
-top: 40%;
+top: 35%;
 bottom: 50%;
+padding-left: 5px;
 
 /* H4 */
 
@@ -126,12 +159,38 @@ line-height: 38px;
 text-align: center;
 letter-spacing: 0.2px;
 
-@media (max-width: 768px){
-    text-size: auto;
-    left:2%;
-    right: 2%;
-    top: 40%;
-    bottom: 50%;
+color: #F7F7F7;
+
+@media (max-width:415px){
+position: static;
+left: 2.76%;
+right: 2.76%;
+top: 10.42%;
+bottom: 10.42%;
+
+/* H4 */
+
+font-family: Montserrat;
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 30px;
+/* or 158% */
+
+text-align: center;
+letter-spacing: 0.2px;
+
+/* second text */
+
+color: #737373;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+margin: 0px 10px;
 }
 
 color: #F7F7F7;
@@ -156,45 +215,58 @@ letter-spacing: 0.2px;
 
 color: #FFFFFF;
 
-@media (max-width: 768px){
-    text-size: auto;
-    left: 5%;
-    right: 5%;
-    top: 2%;
-    bottom: 5%;
-    bottom: 30px;
+@media (max-width:415px){
+    position: static;
+    padding-bottom: 15px;
+    left: 2.43%;
+    right: 2.43%;
+    top: 6.76%;
+    bottom: 6.76%;
+    // padding-left: 11.5px;
+    // padding-rigth: 11.5px;
+    
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 42px;
+    /* or 118% */
+    
+    text-align: center;
+    letter-spacing: 0.2px;
+    
+    /* text */
+    
+    color: #252B42;
+    
+    
+    /* Inside auto layout */
+    
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 0px 10px;
 }
 `;
 
 const PhoneImage1 = styled.img`
 // position: absolute;
-width: 786px;
-padding-top: 0%;
-padding-left: 30%;
-right: 0%
-top: 0%;
-width: 470px;
-height: 800px;
+padding-top: 20px;
+display: block;
+height: 700px;
+width: 400px;
+left: 770px;
+top: 400px;
+
+@media (max-width: 415px){
+padding-top: 40px;
+height: 650px;
+width: 350px;
 left: 430px;
-top: 600px;
-
-background: url(.png);
-/* phone mock up drop shadow */
-filter: drop-shadow(50px 30px 150px rgba(46, 4, 4, 0.21));
-`;
-
-const PhoneImage2 = styled.img`
-// position: absolute;
-width: 285px;
-height: 531px;
-left: 550px;
-top: 1px;
-
-@media (max-width: 768px){
-    display: block;
+top: 695px;
 }
-background: url(.png);
 `;
+
 
 const BannerImage = styled.img`
 // padding-top: 155px;
@@ -204,12 +276,10 @@ height: 619px;
 left: 0px;
 top: -1px;
 
-@media (max-width: 768px){
-    width: 786px;
-    left: 0%;
-    right: 0%
-    top: 0%;
+@media (max-width: 415px){
+    display: none;
 }
+
 // background: linear-gradient(0deg, rgba(23, 16, 16, 0.6), rgba(23, 16, 16, 0.6)), url(.jpg);
 `;
 
