ea996f826a66163a9e9a2a9de3b8224b39cbb099
YathaarthSuri <yathaarthsuri@gmail.com>
2022-02-17 11:43:28 +0530

    Added 2 more components

--- src/pages/home/AppWorking.js
+++ src/pages/home/AppWorking.js
@@ -6,7 +6,7 @@ const AppWorking = () => {
         <div>
             <Heading>How the App Works</Heading>
             <Rectangle>
-                <Pic src="/assets/images/Rectangle.png" alt="phone-image"></Pic>
+                <Pic src="/assets/images/Rectangle1.png" alt="phone-image"></Pic>
                 <Content>
                     <P1>Create an Account</P1>
                     <br />
@@ -15,6 +15,26 @@ const AppWorking = () => {
                     <P3>An account is created with your email and a desired password</P3>
                 </Content>
             </Rectangle>
+            <Rectangle2>
+                <Pic src="/assets/images/Rectangle2.png" alt="phone-image"></Pic>
+                <Content2>
+                    <P1>Explore varieties</P1>
+                    <br />
+                    <P2>Shop for your favorites meal as e dey hot.</P2>
+                    <br />
+                    <P3>Shop for your favorite meals or drinks and enjoy while doing it.</P3>
+                </Content2>
+            </Rectangle2>
+             <Rectangle2>
+                <Pic src="/assets/images/Rectangle3.png" alt="phone-image"></Pic>
+                <Content2>
+                    <P1>Checkout</P1>
+                    <br />
+                    <P2>When you done check out and get it delivered.</P2>
+                    <br />
+                    <P3>When you done check out and get it delivered with ease.</P3>
+                </Content2>
+            </Rectangle2>
         </div>
         
     )
@@ -22,18 +42,19 @@ const AppWorking = () => {
 
 const Pic = styled.img`
 
-    max-width: 100%;
+    max-width: 100vw;
 
     @media (max-width: 768px){
-        width: 302px;
-        height: 525px;
+        width: 650px;
+        height: 581px;
     }
 
     @media (max-width: 414px){
         width: 650px;
-        height: 781px;
+        height: 581px;
         /* margin-left: 10px; */
-        padding-left: 50px;
+        padding-left: 40px;
+        margin-top: -150px;
     }
 
 `;
@@ -46,8 +67,35 @@ const Content= styled.div`
     @media (max-width: 414px){
         text-align: center;
         /* margin-left: 200px; */
-        padding: 0 30px;
-        margin-top: -100px;
+        border-top: 1px solid grey;
+        padding: 50px 30px;
+        /* margin-top: -100px; */
+    }
+
+    @media (min-width: 768px){
+        padding-right: 50px;
+        margin-top: 80px;
+        margin-bottom: 50px;
+    }
+
+    @media (min-width: 1024px){
+        padding-top: 122px;
+        padding-right: 154px;
+    }
+
+`;
+
+const Content2= styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    @media (max-width: 414px){
+        text-align: center;
+        /* margin-left: 200px; */
+        /* border-top: 1px solid grey; */
+        padding: 50px 30px;
+        /* margin-top: -100px; */
     }
 
     @media (min-width: 768px){
@@ -77,6 +125,10 @@ const Heading = styled.h1`
    
 
     margin-top: 616px;
+
+    @media (max-width: 414px){
+        margin-bottom: -70px;
+    }
     /* margin-left: 100px; */
 
 `;
@@ -91,7 +143,26 @@ const Rectangle = styled.div`
     background: #FFFFFF;
 
     @media (max-width: 414px){
-        display: block;
+        flex-direction: column-reverse;
+        margin-top: 144px;
+        /* display: block; */
+    }
+
+`;
+
+const Rectangle2 = styled.div`
+
+    display: flex;  
+    justify-content: center;
+    /* flex-direction: column;
+    align-items: center; */
+
+    background: #FFFFFF;
+
+    @media (max-width: 414px){
+        flex-direction: column-reverse;
+        margin-top: -120px;
+        /* display: block; */
     }
 
 `;
